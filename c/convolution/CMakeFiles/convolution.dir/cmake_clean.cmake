c02eeb6702e04abc92de58b23a0586507722cf17
Rama Rahardi <rama.orion@gmail.com>
2022-06-23 14:34:01 +0700

    Add CMake config

---
+++ c/convolution/CMakeFiles/convolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convolution.dir/convolution.c.o"
+  "convolution"
+  "convolution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/convolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c/realtime_fir/CMakeFiles/realtime_fir.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realtime_fir.dir/realtime_fir.c.o"
+  "realtime_fir"
+  "realtime_fir.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/realtime_fir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
